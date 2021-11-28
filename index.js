0a934c6b7ea895684eca6f5b97de1dc289e63c73
AnhNguyen2580 <84497927+AnhNguyen2580@users.noreply.github.com>
2021-11-28 14:16:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ if (!dev) {
 
 // ================== Connect mongodb with mongoose ==================
 const mongoose = require('mongoose');
-const MONGO_URL = dev ? process.env.MONGO_URL_LOCAL : process.env.MONGO_URL;
+const MONGO_URL = dev ? process.env.MONGO_URL : process.env.MONGO_URL;
 
 mongoose.connect(MONGO_URL, {
   useUnifiedTopology: true,
